5e9b50aedfe9acae7ea5e2eb28c2c595f86056f9
adithyahashen111-dot <adithyahashen111@gmail.com>
2026-09-25 09:58:38 +0530

    Update pair.js

--- pair.js
+++ pair.js
@@ -81,11 +81,11 @@ router.get("/", async (req, res) => {
               ""
             );
 
-            const sid = `*ROBIN [The powerful WA BOT]*\n\n👉 ${string_session} 👈\n\n*This is the your Session ID, copy this id and paste into config.js file*\n\n*You can ask any question using this link*\n\n*wa.me/message/WKGLBR2PCETWD1*\n\n*You can join my whatsapp group*\n\n*https://chat.whatsapp.com/GAOhr0qNK7KEvJwbenGivZ*`;
+            const sid = `*▄︻デ𓆩 𓊈𒆜 ђ𝐀丂ｈᗴ𝕟 𝕄𝐎𝓥ίє βⓄ𝓉 𒆜𓊉 𓆪══━一\n\n👉 ${string_session} 👈\n\n*This is your Session ID. Copy and paste this code into your config.js file.*\n\n*Join Our Official Whatsapp Group:*\nhttps://chat.whatsapp.com/DY95KwnMe3LAB9YJFiHkrg*`;
             const mg = `🛑 *Do not share this code to anyone* 🛑`;
             const dt = await RobinPairWeb.sendMessage(user_jid, {
               image: {
-                url: "https://raw.githubusercontent.com/Dark-Robin/Bot-Helper/refs/heads/main/autoimage/Bot%20robin%20WP.jpg",
+                url: "https://cdn.wallpapersafari.com/16/57/jrIgRf.jpg",
               },
               caption: sid,
             });
